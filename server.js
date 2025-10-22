7177b444b54cb2c6e0779b9464710b4dc9ef7977
Gayathri25-tirumalashetti <vishnupriyathella111@gmail.com>
2025-10-22 15:57:59 +0530

    Initial project commit

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 const books = [
-  { id: 1, title: "Backend", author: "Priya" },
-  { id: 2, title: "Frontend", author: "Yamini" },
-  { id: 3, title: "Sears", author: "Vishnu" },
-  { id: 4, title: "StudentHub", author: "Meghana"},
-  { id: 5, title: "Motu and patluu", author: "Bhooms"},
-  { id: 6, title: "Tom and jerry", author: "Shamu"},
-  { id: 7, title: "Planzo", author: "Bhanu"},
-  { id: 8, title: "Seatease", author: "Yams"},
-  { id: 9, title: "Magicland", author: "Sameena"},
-  { id: 10, title: "Lost and Found", author: "Vissu"}
+  { id: 1, title: "PlanZo", author: "Yamini" },
+  { id: 2, title: "LostAndFound", author: "Vishnu" },
+  { id: 3, title: "Sears", author: "Priya" },
+  { id: 4, title: "Resume", author: "Syamala"},
+  { id: 5, title: "Zest", author: "Bhavya"},
+  { id: 6, title: "Vignan", author: "Bhoomika"},
+  { id: 7, title: "Analyser", author: "Oshitha"},
+  { id: 8, title: "Event", author: "Uma"},
+  { id: 9, title: "Holiday", author: "Koumudi"},
+  { id: 10, title: "Founder", author: "Poojitha"}
 
 ];
 app.get('/books', (req, res) => {
